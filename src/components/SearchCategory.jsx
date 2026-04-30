13e46b90e7489c1c0d3f57d683a692d2bbbd73fa
Hasib <hasibhsb19@gmail.com>
2026-04-30 18:49:05 +0600

    add bettterAuth initial setUp and google id and secret for google auth

--- src/components/SearchCategory.jsx
+++ src/components/SearchCategory.jsx
@@ -1,11 +1,12 @@
 'use client'
 
 import { IoSearchSharp } from "react-icons/io5";
-const SearchCategoryPage = ({ tiles, setSearchTerm }) => {
+import Category from "./CategoryPage";
+const SearchCategoryPage = ({ tiles, setSearchTerm }) => {  
 
   return (
     <div className="w-full flex flex-col items-center px-4">
-      <div className="w-full max-w-2xl mb-10">
+      <div className="max-w-xl md:w-2xl mb-10">
         <div className="flex items-center  bg-[##EFF0E1] rounded-full p-2 pl-6 shadow-sm border border-black/5">
           <IoSearchSharp className="text-[#4B635B] mr-2" size={23} />
           <input
@@ -20,19 +21,8 @@ const SearchCategoryPage = ({ tiles, setSearchTerm }) => {
         </div>
       </div>
 
-      <div className="flex flex-wrap items-center justify-center w-full gap-4 ">
-        <button className="px-6 py-1.5 bg-[#D4E4D4] transition] rounded-full text-sm font-medium text-[#424845]">
-          All Category
-        </button>
-        {tiles.map((cat, index) => (
-          <button
-            key={index}
-            className={`px-6 py-1.5 bg-[#E8E9DA] rounded-full text-sm font-medium text-[#424845] hover:bg-[#D4E4D4] transition-all`}
-          >
-            {cat.category}
-          </button>
-        ))}
-      </div>
+      {/* Category btn */}
+      <Category tiles={tiles}/>
     </div>
   );
 };
