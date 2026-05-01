55f0d9c2985273582686a9ddb7d55e0ca3696230
Hasib <hasibhsb19@gmail.com>
2026-05-01 18:15:00 +0600

    add vercel json file

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,8 +1,8 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    // baseURL: "http://localhost:3000"
-    baseURL: "https://ph-assignment-08-gamma.vercel.app"
+    baseURL: "http://localhost:3000"
+    // baseURL: "https://ph-assignment-08-gamma.vercel.app"
 })
 
 export const { signIn, signUp, signOut, useSession } = createAuthClient
